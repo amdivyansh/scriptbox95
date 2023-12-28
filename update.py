864094dbf5f6996e727b775967a568946d04b118
DIVYANSH <128956643+amdivyansh@users.noreply.github.com>
2023-12-28 14:49:24 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,20 +1,23 @@
 import requests
 import zipfile
 import io
-import os
 import re
-def download_latest_release_repository(owner, repo, access_token, new_folder_name):
-    url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
-    headers = {"Authorization": f"token {access_token}"}
+import os
+import shutil
+def download_latest_release_repository():
+    url = f"https://api.github.com/repos/amdivyansh/scriptbox95_source_data/releases/latest"
+    #headers = {"Authorization": f"token {access_token}"}
     print(url)
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     if response.status_code == 200:
         release_info = response.json()
         download_url = release_info['zipball_url']
         
         print(f"Downloading from: {download_url}")
         
-        response = requests.get(download_url, headers=headers)
+        response = requests.get(download_url)
+        if os.path.exists('main'):
+                        shutil.rmtree('main')
         content_disposition = response.headers.get('Content-type')
         z = zipfile.ZipFile(io.BytesIO(response.content))
         z.extractall()
@@ -22,12 +25,10 @@ def download_latest_release_repository(owner, repo, access_token, new_folder_nam
         extracted_folder_name = z.namelist()[0]
         print(extracted_folder_name)
         # Rename the extracted folder to the new name
-        os.rename(extracted_folder_name, new_folder_name)
-        
-        print(f"Repository downloaded and extracted successfully")
+        os.rename(extracted_folder_name, 'main')
+        os.chdir('./main')
+        exec(open('compile.py').read())
+
+        print(f"Updated successfully")
     else:
         print(f"Failed to download repository. Status code: {response.status_code}")
-
-# Usage:
-# Make sure to replace 'owner', 'repo', 'access_token', and 'new_folder_name' with actual values.
-download_latest_release_repository('amdivyansh', 'scriptbox95', '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', 'main2')
